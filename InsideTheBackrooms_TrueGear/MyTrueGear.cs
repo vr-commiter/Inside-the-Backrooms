e6029dadff59f1af584a82365a15d9cc35de12c0
vr-commiter <vr-commiter@qq.com>
2024-11-01 14:54:53 +0800

    init

---
+++ InsideTheBackrooms_TrueGear/MyTrueGear.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System;
+using TrueGearSDK;
+using System.Linq;
+
+
+
+namespace MyTrueGear
+{
+    public class TrueGearMod
+    {
+        private static TrueGearPlayer _player = null;
+
+        private static ManualResetEvent normalHeartbeatMRE = new ManualResetEvent(false);
+        private static ManualResetEvent elevatedHeartbeatMRE = new ManualResetEvent(false);
+        private static ManualResetEvent stressedHeartbeatMRE = new ManualResetEvent(false);
+        private static ManualResetEvent terrifiedHeartbeatMRE = new ManualResetEvent(false);
+        private static ManualResetEvent radiationMRE = new ManualResetEvent(false);
+
+        public TrueGearMod()
+        {
+            _player = new TrueGearPlayer("1987080","Inside The Backrooms");
+            //RegisterFilesFromDisk();
+            _player.Start();
+            new Thread(new ThreadStart(this.NormalHeartBeat)).Start();
+            new Thread(new ThreadStart(this.ElevatedHeartBeat)).Start();
+            new Thread(new ThreadStart(this.StressedHeartBeat)).Start();
+            new Thread(new ThreadStart(this.TerrifiedHeartBeat)).Start();
+            new Thread(new ThreadStart(this.Radiation)).Start();
+        }
+
+        //private void RegisterFilesFromDisk()
+        //{
+        //    FileInfo[] files = new DirectoryInfo(".//Mods//TrueGear")   //  (".//BepInEx//plugins//TrueGear")
+        //            .GetFiles("*.asset_json", SearchOption.AllDirectories);
+
+        //    for (int i = 0; i < files.Length; i++)
+        //    {
+        //        string name = files[i].Name;
+        //        string fullName = files[i].FullName;
+        //        if (name == "." || name == "..")
+        //        {
+        //            continue;
+        //        }
+        //        string jsonStr = File.ReadAllText(fullName);
+        //        JSONNode jSONNode = JSON.Parse(jsonStr);
+        //        EffectObject _curAssetObj = EffectObject.ToObject(jSONNode.AsObject);
+        //        string uuidName = Path.GetFileNameWithoutExtension(fullName);
+        //        _curAssetObj.uuid = uuidName;
+        //        _curAssetObj.name = uuidName;
+        //        _player.SetupRegister(uuidName, jsonStr);
+        //    }
+        //}
+
+        public void NormalHeartBeat()
+        {
+            while (true)
+            {
+                normalHeartbeatMRE.WaitOne();
+                _player.SendPlay("NormalHeartBeat");
+                Thread.Sleep(1000);
+            }            
+        }
+
+        public void ElevatedHeartBeat()
+        {
+            while (true)
+            {
+                elevatedHeartbeatMRE.WaitOne();
+                _player.SendPlay("ElevatedHeartBeat");
+                Thread.Sleep(600);
+            }
+        }
+
+        public void StressedHeartBeat()
+        {
+            while (true)
+            {
+                stressedHeartbeatMRE.WaitOne();
+                _player.SendPlay("StressedHeartBeat");
+                Thread.Sleep(450);
+            }
+        }
+
+        public void TerrifiedHeartBeat()
+        {
+            while (true)
+            {
+                terrifiedHeartbeatMRE.WaitOne();
+                _player.SendPlay("TerrifiedHeartBeat");
+                Thread.Sleep(250);
+            }
+        }
+
+        public void Radiation()
+        {
+            while (true)
+            {
+                radiationMRE.WaitOne();
+                _player.SendPlay("Radiation");
+                Thread.Sleep(500);
+            }
+        }
+
+        public void Play(string Event)
+        { 
+            _player.SendPlay(Event);
+        }
+
+        //public void PlayAngle(string tmpEvent, float tmpAngle, float tmpVertical)
+        //{
+        //    try
+        //    {
+        //        float angle = (tmpAngle - 22.5f) > 0f ? tmpAngle - 22.5f : 360f - tmpAngle;
+        //        int horCount = (int)(angle / 45) + 1;
+
+        //        int verCount = tmpVertical > 0.1f ? -4 : tmpVertical < 0f ? 8 : 0;
+
+        //        EffectObject originalObject = _player.FindEffectByUuid(tmpEvent);
+        //        EffectObject rootObject = EffectObject.Copy(originalObject);
+
+
+        //        foreach (TrackObject track in rootObject.trackList)
+        //        {
+        //            if (track.action_type == ActionType.Shake)
+        //            {
+        //                for (int i = 0; i < track.index.Length; i++)
+        //                {
+        //                    if (verCount != 0)
+        //                    {
+        //                        track.index[i] += verCount;
+        //                    }
+        //                    if (horCount < 8)
+        //                    {
+        //                        if (track.index[i] < 50)
+        //                        {
+        //                            int remainder = track.index[i] % 4;
+        //                            if (horCount <= remainder)
+        //                            {
+        //                                track.index[i] = track.index[i] - horCount;
+        //                            }
+        //                            else if (horCount <= (remainder + 4))
+        //                            {
+        //                                var num1 = horCount - remainder;
+        //                                track.index[i] = track.index[i] - remainder + 99 + num1;
+        //                            }
+        //                            else
+        //                            {
+        //                                track.index[i] = track.index[i] + 2;
+        //                            }
+        //                        }
+        //                        else
+        //                        {
+        //                            int remainder = 3 - (track.index[i] % 4);
+        //                            if (horCount <= remainder)
+        //                            {
+        //                                track.index[i] = track.index[i] + horCount;
+        //                            }
+        //                            else if (horCount <= (remainder + 4))
+        //                            {
+        //                                var num1 = horCount - remainder;
+        //                                track.index[i] = track.index[i] + remainder - 99 - num1;
+        //                            }
+        //                            else
+        //                            {
+        //                                track.index[i] = track.index[i] - 2;
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //                if (track.index != null)
+        //                {
+        //                    track.index = track.index.Where(i => !(i < 0 || (i > 19 && i < 100) || i > 119)).ToArray();
+        //                }
+        //            }
+        //            else if (track.action_type == ActionType.Electrical)
+        //            {
+        //                for (int i = 0; i < track.index.Length; i++)
+        //                {
+        //                    if (horCount <= 4)
+        //                    {
+        //                        track.index[i] = 0;
+        //                    }
+        //                    else
+        //                    {
+        //                        track.index[i] = 100;
+        //                    }
+        //                    if (horCount == 1 || horCount == 8 || horCount == 4 || horCount == 5)
+        //                    {
+        //                        track.index = new int[2] { 0, 100 };
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        _player.SendPlayEffectByContent(rootObject);
+        //    }
+        //    catch(Exception ex)
+        //    { 
+        //        Console.WriteLine("TrueGear Mod PlayAngle Error :" + ex.Message);
+        //        _player.SendPlay(tmpEvent);
+        //    }          
+        //}
+
+        public void StartNormalHeartBeat()
+        {
+            normalHeartbeatMRE.Set();
+        }
+
+        public void StopNormalHeartBeat()
+        {
+            normalHeartbeatMRE.Reset();
+        }
+
+        public void StartElevatedHeartBeat()
+        {
+            elevatedHeartbeatMRE.Set();
+        }
+
+        public void StopElevatedHeartBeat()
+        {
+            elevatedHeartbeatMRE.Reset();
+        }
+
+        public void StartTerrifiedHeartBeat()
+        {
+            terrifiedHeartbeatMRE.Set();
+        }
+
+        public void StopTerrifiedHeartBeat()
+        {
+            terrifiedHeartbeatMRE.Reset();
+        }
+
+        public void StartStressedHeartBeat()
+        {
+            stressedHeartbeatMRE.Set();
+        }
+
+        public void StopStressedHeartBeat()
+        {
+            stressedHeartbeatMRE.Reset();
+        }
+
+        public void StartRadiation()
+        {
+            radiationMRE.Set();
+        }
+
+        public void StopRadiation()
+        {
+            radiationMRE.Reset();
+        }
+
+
+
+    }
+}
---
+++ InsideTheBackrooms_TrueGear/Plugin.cs
@@ -0,0 +1,357 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using MyTrueGear;
+using System.Threading;
+
+namespace InsideTheBackrooms_TrueGear
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        internal static new ManualLogSource Log;
+
+        private static TrueGearMod _TrueGear = null;
+
+        private static HeartbeatType heartbeatType;
+        private static int playerID;
+        private static bool isRadiation = false;
+        private static float anxietyValue = 0;
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log = base.Log;
+
+            HarmonyLib.Harmony.CreateAndPatchAll(typeof(Plugin));
+            _TrueGear = new TrueGearMod();
+            _TrueGear.Play("HeartBeat");
+
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerInventory), "TargetOnItemPickup")]
+        private static void PlayerInventory_TargetOnItemPickup_Postfix(PlayerInventory __instance)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("TargetOnItemPickup");
+            _TrueGear.Play("BackSlotInputItem");
+
+        }
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerInventory), "DropItem")]
+        private static void PlayerInventory_DropItem_Postfix(PlayerInventory __instance)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("DropItem");
+            _TrueGear.Play("ChestSlotOutputItem");
+        }
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "ServerTakeDamage")]
+        private static void PlayerStats_ServerTakeDamage_Postfix(PlayerStats __instance)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("ServerTakeDamage");
+            _TrueGear.Play("PoisonDamage");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "Update")]
+        private static void PlayerStats_Update_Postfix(PlayerStats __instance)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            if (__instance.Radiation > 25f && !isRadiation)
+            {
+                isRadiation = true;
+                Log.LogInfo("--------------------------------------");
+                Log.LogInfo("StartRadiation");
+                _TrueGear.StartRadiation();
+                Log.LogInfo(__instance.Radiation);
+            }
+            else if (__instance.Radiation <= 25f && isRadiation)
+            {
+                isRadiation = false;
+                Log.LogInfo("--------------------------------------");
+                Log.LogInfo("StopRadiation");
+                _TrueGear.StopRadiation();
+                Log.LogInfo(__instance.Radiation);
+            }
+            if (anxietyValue > __instance.anxiety + 20)
+            {
+                Log.LogInfo("--------------------------------------");
+                Log.LogInfo("DecAnxiety");
+                _TrueGear.Play("Eating");
+                Log.LogInfo(anxietyValue);
+                Log.LogInfo(__instance.anxiety);
+            }
+            anxietyValue = __instance.anxiety;
+
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "ServerHeal")]
+        private static void PlayerStats_ServerHeal_Postfix(PlayerStats __instance, float healthPoints)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("ServerHeal");
+            _TrueGear.Play("Healing");
+            Log.LogInfo(healthPoints);
+        }
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "ServerEnableBoost")]
+        private static void PlayerStats_ServerEnableBoost_Postfix(PlayerStats __instance)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("ServerEnableBoost");
+        }
+
+        private static bool canAddAnxiety = true;
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "ServerAddAnxiety")]
+        private static void PlayerStats_ServerAddAnxiety_Postfix(PlayerStats __instance, float amount)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            if (!canAddAnxiety)
+            {
+                return;
+            }
+            canAddAnxiety = false;
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("ServerAddAnxiety");
+            _TrueGear.Play("AddAnxiety");
+            Log.LogInfo(amount);
+            Timer addAnxietyTimer = new Timer(AddAnxietyTimerCallBack, null, 110, Timeout.Infinite);
+        }
+
+        private static void AddAnxietyTimerCallBack(object o)
+        {
+            canAddAnxiety = true;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(HeartbeatAudio), "SetHeartbeat")]
+        private static void HeartbeatAudio_SetHeartbeat_Postfix(HeartbeatAudio __instance, HeartbeatType heartbeat)
+        {
+            if (heartbeatType == heartbeat)
+            {
+                return;
+            }
+            heartbeatType = heartbeat;
+
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("SetHeartbeat");
+            Log.LogInfo(heartbeat);
+            _TrueGear.StopNormalHeartBeat();
+            _TrueGear.StopElevatedHeartBeat();
+            _TrueGear.StopStressedHeartBeat();
+            _TrueGear.StopTerrifiedHeartBeat();
+            switch (heartbeat)
+            {
+                case HeartbeatType.NORMAL:
+                    _TrueGear.StartNormalHeartBeat();
+                    break;
+                case HeartbeatType.ELEVATED:
+                    _TrueGear.StartElevatedHeartBeat();
+                    break;
+                case HeartbeatType.STRESSED:
+                    _TrueGear.StartStressedHeartBeat();
+                    break;
+                case HeartbeatType.TERRIFIED:
+                    _TrueGear.StartTerrifiedHeartBeat();
+                    break;
+                case HeartbeatType.NONE:
+                default:
+                    _TrueGear.StopNormalHeartBeat();
+                    _TrueGear.StopElevatedHeartBeat();
+                    _TrueGear.StopStressedHeartBeat();
+                    _TrueGear.StopTerrifiedHeartBeat();
+                    break;
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "KillPlayer")]
+        private static void PlayerStats_KillPlayer_Postfix(PlayerStats __instance)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("PLayerDeath");
+            Log.LogInfo("StopRadiation");
+            _TrueGear.Play("PlayerDeath");
+            _TrueGear.StopRadiation();
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "OnDieStateChange")]
+        private static void PlayerStats_OnDieStateChange_Postfix(PlayerStats __instance, bool o, bool n)
+        {
+            if (playerID != __instance.components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            if (!n)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("OnDieStateChange");
+            Log.LogInfo("StopRadiation");
+            _TrueGear.Play("PlayerDeath");
+            _TrueGear.StopRadiation();
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerStats), "OnStartLocalPlayer")]
+        private static void PlayerStats_OnStartLocalPlayer_Postfix(PlayerStats __instance)
+        {
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("OnStartLocalPlayer");
+            _TrueGear.Play("LevelStarted");
+            isRadiation = false;
+            playerID = __instance.components.Info.Networkm_PlayerID;
+            Log.LogInfo(playerID);
+        }
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerGear), "OnFlashlightUse")]
+        private static void PlayerGear_OnFlashlightUse_Postfix(PlayerGear __instance, bool o, bool n)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("OnFlashlightUse");
+            _TrueGear.Play("RightHandPickupItem");
+            Log.LogInfo(o);
+            Log.LogInfo(n);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerGear), "OnSuitEquip")]
+        private static void PlayerGear_OnSuitEquip_Postfix(PlayerGear __instance, bool o, bool n)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("OnSuitEquip");
+            Log.LogInfo(o);
+            Log.LogInfo(n);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerGear), "OnToolEquiped")]
+        private static void PlayerGear_OnToolEquiped_Postfix(PlayerGear __instance, PlayerGear.GearTool old, PlayerGear.GearTool newTool)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            if (newTool == PlayerGear.GearTool.NONE)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("OnToolEquiped");
+            _TrueGear.Play("RightHandPickupItem");
+            Log.LogInfo(old);
+            Log.LogInfo(newTool);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerGear), "Target_DrinkAlmondWater")]
+        private static void PlayerGear_Target_DrinkAlmondWater_Postfix(PlayerGear __instance)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("Target_DrinkAlmondWater");
+            _TrueGear.Play("Drinking");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerGear), "Target_ConsumeJellyJar")]
+        private static void PlayerGear_Target_ConsumeJellyJar_Postfix(PlayerGear __instance)
+        {
+            if (playerID != __instance.Components.Info.Networkm_PlayerID)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("Target_ConsumeJellyJar");
+            _TrueGear.Play("Eating");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(BasePlayerController), "TargetExitFromHider")]
+        private static void BasePlayerController_TargetExitFromHider_Postfix(BasePlayerController __instance)
+        {
+            if (!__instance.isLocalPlayer)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("TargetExitFromHider");
+            _TrueGear.Play("RightHandPickupItem");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(BasePlayerController), "TargetUseHiderProp")]
+        private static void BasePlayerController_TargetUseHiderProp_Postfix(BasePlayerController __instance)
+        {
+            if (!__instance.isLocalPlayer)
+            {
+                return;
+            }
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("TargetUseHiderProp");
+            _TrueGear.Play("RightHandPickupItem");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerController), "PlayMonsterDeathAnimation")]
+        private static void PlayerController_PlayMonsterDeathAnimation_Postfix(PlayerController __instance)
+        {
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("PlayMonsterDeathAnimation");
+            _TrueGear.Play("MonsterDeath");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PauseMenuUI), "ExitBtn_Click")]
+        private static void PauseMenuUI_ExitBtn_Click_Postfix(PauseMenuUI __instance)
+        {
+            Log.LogInfo("--------------------------------------");
+            Log.LogInfo("Exit");
+            _TrueGear.StopRadiation();
+            _TrueGear.StopNormalHeartBeat();
+            _TrueGear.StopElevatedHeartBeat();
+            _TrueGear.StopStressedHeartBeat();
+            _TrueGear.StopTerrifiedHeartBeat();
+        }
+
+    }
+}
